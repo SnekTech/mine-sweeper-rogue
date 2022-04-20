47630d8446132ef6c6b1bfdb724b92c024b14c45
syf <2654525303@qq.com>
2022-04-21 03:49:19 +0800

    add static class Utils for common utilities

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SnekTech
+{
+    public static class Utils
+    {
+        public static Vector3 GetMouseWorldPosition(Vector2 screenPosition, Camera camera)
+        {
+            return camera.ScreenToWorldPoint(screenPosition);
+        }
+
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector2 screenPosition = Mouse.current.position.ReadValue();
+            Camera mainCamera = Camera.main;
+            return GetMouseWorldPosition(screenPosition, mainCamera);
+        }
+    }
+}
