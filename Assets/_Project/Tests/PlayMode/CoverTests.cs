d9823693e740241f911dcc8ad4565b5423826f69
syf <2654525303@qq.com>
2022-05-02 00:32:20 +0800

    adjust cover tests and flag tests - add SetUp() and TearDown - use Assert.That instead of classic nunit assertion

--- Assets/_Project/Tests/PlayMode/CoverTests.cs
+++ Assets/_Project/Tests/PlayMode/CoverTests.cs
@@ -3,29 +3,44 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using SnekTech.GridCell;
 using Tests.PlayMode.Builder;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Tests.PlayMode
 {
     public class CoverTests
     {
+        private CoverBehaviour _coverBehaviour;
+        private ICover _cover;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _coverBehaviour = A.CoverBehaviour;
+            _cover = _coverBehaviour;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_coverBehaviour.gameObject);
+        }
+        
         [UnityTest]
         public IEnumerator cell_animation_complete_events_should_emit()
         {
-            CoverBehaviour coverBehaviour = A.CoverBehaviour;
-            ICover cover = coverBehaviour;
 
             bool revealedCompletedCalled = false, putCoverCompletedCalled = false;
-            cover.RevealCompleted += () => revealedCompletedCalled = true;
-            cover.PutCoverCompleted += () => putCoverCompletedCalled = true;
+            _cover.RevealCompleted += () => revealedCompletedCalled = true;
+            _cover.PutCoverCompleted += () => putCoverCompletedCalled = true;
 
             async Task Run()
             {
-                await cover.RevealAsync();
-                await cover.PutCoverAsync();
+                await _cover.RevealAsync();
+                await _cover.PutCoverAsync();
                 
-                Assert.IsTrue(revealedCompletedCalled);
-                Assert.IsTrue(putCoverCompletedCalled);
+                Assert.That(revealedCompletedCalled, Is.True);
+                Assert.That(putCoverCompletedCalled, Is.True);
             }
 
             yield return Run().AsCoroutine();
--- Assets/_Project/Tests/PlayMode/FlagTests.cs
+++ Assets/_Project/Tests/PlayMode/FlagTests.cs
@@ -3,31 +3,46 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using SnekTech.GridCell;
 using Tests.PlayMode.Builder;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Tests.PlayMode
 {
     public class FlagTests
     {
+        private FlagBehaviour _flagBehaviour;
+        private IFlag _flag;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _flagBehaviour = A.FlagBehaviour;
+            _flag = _flagBehaviour;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_flagBehaviour.gameObject);
+        }
+
         [UnityTest]
         public IEnumerator flag_animation_complete_events_should_emit()
         {
-            FlagBehaviour flagBehaviour = A.FlagBehaviour;
-            IFlag flag = flagBehaviour;
-
             bool liftEventInvoked = false, putDownEventInvoked = false;
-            flag.LiftCompleted += () => liftEventInvoked = true;
-            flag.PutDownCompleted += () => putDownEventInvoked = true;
-            
-            yield return Run().AsCoroutine();
+            _flag.LiftCompleted += () => liftEventInvoked = true;
+            _flag.PutDownCompleted += () => putDownEventInvoked = true;
+
             async Task Run()
             {
-                await flag.LiftAsync();
-                await flag.PutDownAsync();
-                
-                Assert.IsTrue(liftEventInvoked);
-                Assert.IsTrue(putDownEventInvoked);
+                await _flag.LiftAsync();
+                await _flag.PutDownAsync();
+
+                Assert.That(liftEventInvoked, Is.True);
+                Assert.That(putDownEventInvoked, Is.True);
             }
+
+            yield return Run().AsCoroutine();
         }
     }
 }
