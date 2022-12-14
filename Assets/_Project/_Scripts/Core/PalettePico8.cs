a41c2fb49a0c990256dd04121a81c13862f5752e
syf <2654525303@qq.com>
2022-12-14 20:17:42 +0800

    add PICO-8 palette

---
+++ Assets/_Project/_Scripts/Core/PalettePico8.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SnekTech.Core
+{
+    public static class PalettePico8
+    {
+        public static Color Black = Color.black;
+        public static Color DarkBlue = new Color(0.11f, 0.17f, 0.33f);
+        public static Color DarkPurple = new Color(0.49f, 0.15f, 0.33f);
+        public static Color DarkGreen = new Color(0f, 0.53f, 0.32f);
+        public static Color Brown = new Color(0.67f, 0.32f, 0.21f);
+        public static Color DarkGray = new Color(0.37f, 0.34f, 0.31f);
+        public static Color LightGray = new Color(0.76f, 0.76f, 0.78f);
+        public static Color White = new Color(1f, 0.95f, 0.91f);
+        public static Color Red = new Color(1f, 0f, 0.3f);
+        public static Color Orange = new Color(1f, 0.64f, 0f);
+        public static Color Yellow = new Color(1f, 0.93f, 0.15f);
+        public static Color Green = new Color(0f, 0.89f, 0.21f);
+        public static Color Blue = new Color(0.16f, 0.68f, 1f);
+        public static Color Indigo = new Color(0.51f, 0.46f, 0.61f);
+        public static Color Pink = new Color(1f, 0.47f, 0.66f);
+        public static Color Peach = new Color(1f, 0.8f, 0.67f);
+    }
+}
