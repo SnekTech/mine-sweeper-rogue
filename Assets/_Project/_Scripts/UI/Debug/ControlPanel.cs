89ad6fdedd2008e2febba15fcedb92ac79999ed3
syf <2654525303@qq.com>
2022-12-15 17:45:50 +0800

    add a debug panel

---
+++ Assets/_Project/_Scripts/UI/Debug/ControlPanel.cs
@@ -0,0 +1,28 @@
+﻿using SnekTech.Player;
+using UnityEngine;
+
+namespace SnekTech.UI.Debug
+{
+    public class ControlPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerState playerState;
+
+        [Header("Health")]
+        [SerializeField]
+        private int healthIncrement = 1;
+
+        [SerializeField]
+        private int damageAmount = 1;
+
+        public void OnAddHealthButtonClick()
+        {
+            playerState.AddHealth(healthIncrement).Forget();
+        }
+
+        public void OnTakeDamageButtonClick()
+        {
+            playerState.TakeDamage(damageAmount).Forget();
+        }
+    }
+}
